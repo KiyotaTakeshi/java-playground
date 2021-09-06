ea0f65aa9afd1d529daaa40464f76ad062b1c294
Takeshi Kiyota <t.kiyota@gxp.co.jp>
2021-09-07 08:50:44 +0900

    collection

--- src/com/kiyotakeshi/Collection/Main.java
+++ src/com/kiyotakeshi/Collection/Main.java
@@ -1,6 +1,7 @@
 package com.kiyotakeshi.Collection;
 
 import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +12,14 @@ public class Main {
         // ・キーと値を分けて要素を扱いたい場合は Map interface
 
         // List
+        // ArrayList は内部に配列を利用しているため
+        // index を指定して要素を代入や取得は高速に行える
+        // 末尾に要素を追加する処理も高速で行えるが、
+        // 配列の途中に要素を追加しようとするとそれ以降のすべてを後ろにずらすため時間がかかる
+
+        // LinkedList は要素自身が前後の要素の情報を持つ
+        // index を指定した取得は先頭からリストを順番にたどるため時間がかかるが、
+        // リストの途中に要素を高速に追加、削除できる
         List<Integer> integerList = new ArrayList<>(Arrays.asList(1, 38, 29, 24, 3));
         integerList.add(65);
         System.out.println("integerList:" + integerList);
@@ -30,14 +39,6 @@ public class Main {
         System.out.println(Collections.binarySearch(integerList, 3));
 
         System.out.println("----------------");
-        // ArrayList は内部に配列を利用しているため
-        // index を指定して要素を代入や取得は高速に行える
-        // 末尾に要素を追加する処理も高速で行えるが、
-        // 配列の途中に要素を追加しようとするとそれ以降のすべてを後ろにずらすため時間がかかる
-
-        // LinkedList は要素自身が前後の要素の情報を持つ
-        // index を指定した取得は先頭からリストを順番にたどるため時間がかかるが、
-        // リストの途中に要素を高速に追加、削除できる
         List<Student> students = new ArrayList<>();
         students.add(new Student("mike", 80));
         students.add(new Student("popcorn", 78));
@@ -55,5 +56,54 @@ public class Main {
         System.out.println(students);
 
         System.out.println("----------------");
+
+        // Map
+        // HashMap
+        // key からハッシュ値を計算して(key のオブジェクトの hashCode メソッドを使用)、
+        // 内部のハッシュテーブルに key と value を格納する
+        // key が異なっても hashCode がおなじになる場合は equals メソッドを用いて一致する要素を検索
+
+        // LinkedHashMap は要素自身が前後の情報を持っている
+        HashMap<String, Integer> scores = new HashMap<>();
+        scores.put("Mike", 88);
+        scores.put("Popcorn", 68);
+        scores.put("Taro", 60);
+        System.out.println(scores);
+
+        // get だと存在しない場合、値として null を持っている場合に null が返ってくるので
+        // kye や value が存在しないことを確認するには contains を使う
+        System.out.println(scores.containsKey("Mike")); // true
+        System.out.println(scores.containsValue(60)); // true
+
+        // static initializer で初期化した map を取得
+        Map<Integer, String> map = Student.getMap();
+        System.out.println(map.get(1));
+
+        // Set
+        // 値の集合を扱うインターフェースで、特定の要素を取得するメソッドは存在しない
+        // Set の内部には Map が存在し、 Set に追加された要素は Map のキーとして保持される
+        System.out.println("----------------");
+        HashSet<String> names = new HashSet<>();
+        names.add("Mike");
+        names.add("Popcorn");
+        names.add("Taro");
+        names.add("Taro");
+        System.out.println(names); // 重複は排除される
+
+        // Queue
+        // FIFO で値を出し入れするコレクション
+        // 外部プログラムからデータが送信された、自分のプログラム側で処理が追いつかない場合に、
+        // バッファとして使用し順番を保持したまま、後でデータを処理できる
+        System.out.println("----------------");
+        Queue<Integer> queue = new ArrayBlockingQueue<>(10);
+        queue.offer(1);
+        queue.offer(20);
+        queue.offer(3);
+        System.out.println(queue);
+        System.out.println(queue.peek()); // 値を削除しない
+        System.out.println(queue.poll());
+        System.out.println(queue);
+
+        // 双方向に値の出し入れができるコレクションが Deque(Double Ended Queue)
     }
 }
--- src/com/kiyotakeshi/Collection/Student.java
+++ src/com/kiyotakeshi/Collection/Student.java
@@ -1,8 +1,19 @@
 package com.kiyotakeshi.Collection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Student {
     private String name;
     private int score;
+
+    // クラスの初期化時に Map も初期化する static initializer
+    private static final Map<Integer, String> map = new HashMap<>();
+    static {
+        map.put(1,"hoge");
+        map.put(2,"fuga");
+        map.put(3,"hogefuga");
+    }
 
     public Student(String name, int score) {
         this.name = name;
@@ -23,5 +34,9 @@ public class Student {
                 "name='" + name + '\'' +
                 ", score=" + score +
                 '}';
+    }
+
+    public static Map<Integer, String> getMap() {
+        return map;
     }
 }
